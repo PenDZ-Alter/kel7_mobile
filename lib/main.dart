fa09e6e86ad361ced11e16e24a52faea5875df91
PenDZ-Alter <abxpendz45@gmail.com>
2024-09-05 22:55:54 +0700

    refactor: switch to oop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tugas1_ui/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,39 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hamilton Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hamilton'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-          ],
-        ),
-      )
+      home: const HomePage(title: "HAMILTON"),
     );
   }
 }
