2f33a348baaf0a63205088525ff27196151457a9
PenDZ-Alter <abxpendz45@gmail.com>
2024-09-06 22:16:38 +0700

    feat: adding scrollbar

--- lib/home.dart
+++ lib/home.dart
@@ -29,165 +29,220 @@ class _HomePageState extends State<HomePage> {
                 style: const TextStyle(fontFamily: 'Times New Roman'),
               ),
             ),
-            Align(alignment: Alignment.centerRight, child: Text('Sign in')),
+            Align(
+              alignment: Alignment.centerRight, 
+              child: Text('Sign in',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400
+                ),
+              )
+            ),
           ],
         ),
         backgroundColor: const Color.fromARGB(255, 218, 179, 6));
 
     /* Edit content here */
     Container content = Container(
-        child: Column(
-      children: <Widget>[
-        // Row for the icons with text labels (Lottery, Treasury, etc.)
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // Lottery
-              Column(
-                children: [
-                  Icon(Icons.bookmark),
-                  SizedBox(height: 8),
-                  Text('Lottery'),
-                ],
-              ),
-              // Treasury
-              Column(
-                children: [
-                  Icon(Icons.star),
-                  SizedBox(height: 8),
-                  Text('Treasury'),
-                ],
-              ),
-              // Trivia
-              Column(
-                children: [
-                  Icon(Icons.help),
-                  SizedBox(height: 8),
-                  Text('Trivia'),
-                ],
-              ),
-              // Karaoke
-              Column(
-                children: [
-                  Icon(Icons.mic),
-                  SizedBox(height: 8),
-                  Text('Karaoke'),
-                ],
-              ),
-              // Hamcam
-              Column(
-                children: [
-                  Icon(Icons.camera_alt),
-                  SizedBox(height: 8),
-                  Text('#hamcam'),
-                ],
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          // New Block of Tickets tile
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Container(
-            color: Colors.black12, // Background color for the container
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      child: Column(
+        children: <Widget>[
+          // Row for the icons with text labels (Lottery, Treasury, etc.)
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 500.0, // Fixed width for the image container
-                  height: 300.0, // Fixed height for the image container
-                  child: Image.asset(
-                    'Images/Hamilton.jpg',
-                    fit: BoxFit.contain, // Ensure the image is not cut off
-                  ),
+                // Lottery
+                Column(
+                  children: [
+                    Icon(Icons.bookmark),
+                    SizedBox(height: 8),
+                    Text('Lottery'),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'August 20',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                // Treasury
+                Column(
+                  children: [
+                    Icon(Icons.star),
+                    SizedBox(height: 8),
+                    Text('Treasury'),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'New Block of Tickets for Hamilton on Broadway Released!',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                // Trivia
+                Column(
+                  children: [
+                    Icon(Icons.help),
+                    SizedBox(height: 8),
+                    Text('Trivia'),
+                  ],
+                ),
+                // Karaoke
+                Column(
+                  children: [
+                    Icon(Icons.mic),
+                    SizedBox(height: 8),
+                    Text('Karaoke'),
+                  ],
+                ),
+                // Hamcam
+                Column(
+                  children: [
+                    Icon(Icons.camera_alt),
+                    SizedBox(height: 8),
+                    Text('#hamcam'),
+                  ],
                 ),
               ],
             ),
           ),
-        ),
+        
+          // Image tiles scrollable horizontal axis
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Container(
+              height: 350.0, // Adjust the height as needed
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  // Tile 1
+                  Container(
+                    width: 300.0,
+                    margin: EdgeInsets.only(right: 8.0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/Images/Hamilton.jpg',
+                          fit: BoxFit.contain,
+                          height: 200,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Tile 1")
+                      ],
+                    ),
+                    color: Colors.blue,
+                  ),
+              
+                  // Tile 2
+                  Container(
+                    width: 200.0,
+                    margin: EdgeInsets.only(right: 8.0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/Images/Hamilton.jpg',
+                          fit: BoxFit.contain,
+                          height: 200,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Tile 1")
+                      ],
+                    ),
+                    color: Colors.blue,
+                  ),
+              
+                  // Tile 3
+                  Container(
+                    width: 200.0,
+                    margin: EdgeInsets.only(right: 8.0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/Images/Hamilton.jpg',
+                          fit: BoxFit.contain,
+                          height: 200,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Tile 1")
+                      ],
+                    ),
+                    color: Colors.blue
+                  ),
 
-        // Eduham Online tile
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Container(
-            color: Colors.black12,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Mobile Programming Online',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                  // Tile 4
+                  Container(
+                    width: 200.0,
+                    margin: EdgeInsets.only(right: 8.0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/Images/Hamilton.jpg',
+                          fit: BoxFit.contain,
+                          height: 200,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Tile 1")
+                      ],
                     ),
+                    color: Colors.blue
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'The UIN Malang Informatic Engineering subject you can do from home!',
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-                // Button
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Your logic here
-                    },
-                    child: Text('Learn More'),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-
-        // The rest of your content
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'This is just a demo',
-            style: TextStyle(
-              fontFamily: 'Arial',
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        
+          // Eduham Online tile
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Container(
+              color: Colors.black12,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Mobile Programming Online',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'The UIN Malang Informatic Engineering subject you can do from home!',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  // Button
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Your logic here
+                      },
+                      child: Text('Learn More'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
-    ));
+        
+          // The rest of your content
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'This is just a demo',
+              style: TextStyle(
+                fontFamily: 'Arial',
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
 
     return Scaffold(
       appBar: AppbarContent,
       body: content,
+      resizeToAvoidBottomInset: false,
     );
   }
 }
