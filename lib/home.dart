4b6c08d6d66530eca5c26aa7414c652b0695dad7
Ditto-Prasetyo <dittoprasetyo100@gmail.com>
2024-09-10 13:08:50 +0700

    feat: Add much more feature

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,9 @@ class _HomePageState extends State<HomePage> {
   double tileWidth = 300.0;
   double tileHeight = 300.0;
   double tileContainerHeight = 450.0;
+  double tileContainerHeights = 200.0;
+  double tileWidths = 150.0;
+  double tileHeights = 150.0;
 
   // Variabel counter untuk menghitung jumlah klik tombol
   int buttonPressCount = 0;
@@ -45,6 +48,12 @@ class _HomePageState extends State<HomePage> {
       "image": "assets/Images/City4.jpg",
       "title": "Pemandangan kota Denmark pada sore hari"
     }
+  ];
+
+  List<Map<String, dynamic>> tilesDatas = [
+    {"images": "assets/Images/satisfied.jpg", "titles": "Satisfied"},
+    {"images": "assets/Images/Hamcam.jpg", "titles": "Share a #HamCam"},
+    {"images": "assets/Images/yayhamlet.jpg", "titles": "Today's Sticker"},
   ];
 
   // Fungsi buildCircularIcon
@@ -105,18 +114,14 @@ class _HomePageState extends State<HomePage> {
         title: Stack(
           children: [
             Align(
-              alignment: Alignment.centerLeft,
-              child: Builder(
-                builder: (context) {
+                alignment: Alignment.centerLeft,
+                child: Builder(builder: (context) {
                   return GestureDetector(
-                    child: Icon(Icons.list),
-                    onTap: () {
-                      Scaffold.of(context).openDrawer();
-                    }
-                  );
-                }
-              )
-            ),
+                      child: Icon(Icons.list),
+                      onTap: () {
+                        Scaffold.of(context).openDrawer();
+                      });
+                })),
             Align(
               alignment: Alignment.center,
               child: Text(
@@ -125,39 +130,37 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Align(
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () {
-                  // Navigasi ke halaman login
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-                child: AnimatedScale(
-                  scale: isAnimating ? 1.2 : 1.0,
-                  duration: Duration(milliseconds: 300),
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                  onEnd: () {
-                    setState(() {
-                      isAnimating = false;
-                    });
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    // Navigasi ke halaman login
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
                   },
-                ),
-              )
-            ),
+                  child: AnimatedScale(
+                    scale: isAnimating ? 1.2 : 1.0,
+                    duration: Duration(milliseconds: 300),
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                    onEnd: () {
+                      setState(() {
+                        isAnimating = false;
+                      });
+                    },
+                  ),
+                )),
           ],
         ),
-        backgroundColor: const Color.fromARGB(255, 218, 179, 6)
-      );
-    
+        backgroundColor: const Color.fromARGB(255, 218, 179, 6));
+
     // Drawer
     Drawer sideMenu = Drawer(
       child: ListView(
@@ -355,14 +358,29 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Mobile Programming Online',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Widget Gambar untuk logo topi wisuda
+                          Image.asset(
+                            'assets/Images/graduation_hat.jpg', // pastikan file logo ada di folder assets
+                            width: 24, // Sesuaikan ukuran gambar
+                            height: 24,
+                          ),
+                          SizedBox(
+                              width:
+                                  10), // Memberikan jarak antara logo dan teks
+                          Text(
+                            'Eduham Online',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
+
                     Padding(
                       padding: EdgeInsets.all(8.0),
                       child: Text(
@@ -379,17 +397,40 @@ class _HomePageState extends State<HomePage> {
                         child: ElevatedButton(
                             onPressed: () {
                               setState(() {
-                                // Setiap tombol dipencet, tambahkan counter
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Center(
+                                          child: Container(
+                                              child: Text("Learn More"))),
+                                      content: Text(
+                                          "Unfortunately, this feature is still on maintenance. Please contact support if the feature is still not working."),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          child: Text(
+                                            "OK",
+                                            style: TextStyle(color: Colors.red),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pop(); // Menutup dialog
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
                                 buttonPressCount++;
                               });
                               print(
                                   "Tombol telah dipencet sebanyak $buttonPressCount kali");
                             },
                             style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white70,
+                              backgroundColor: Colors.white,
                               shadowColor:
                                   Colors.black, // Ubah warna shadow tombol
-                              elevation: 7, // Ubah elevasi tombol
+                              elevation: 8, // Ubah elevasi tombol
                               padding: EdgeInsets.symmetric(
                                   horizontal: 20,
                                   vertical: 10), // Ubah padding tombol
@@ -411,19 +452,215 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
 
-            // Content 4
+            // Image Tiles, Scrollable horizontal axis 2
             Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'This is just a demo',
-                style: TextStyle(
-                  fontFamily: 'Arial',
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Container(
+                height: tileContainerHeights,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 6, // Blur radius
+                      offset: Offset(0, 3), // Position of shadow (x, y)
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: tilesDatas.length,
+                  itemBuilder: (context, i) {
+                    return Container(
+                      width: tileWidths,
+                      margin: EdgeInsets.only(right: 12.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 6.0,
+                          color: Color.fromARGB(0, 0, 0, 0),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10.0),
+                                topRight: Radius.circular(10.0)),
+                            child: Image.asset(
+                              tilesDatas[i]['images'],
+                              fit: BoxFit.cover,
+                              height: tileHeights,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            tilesDatas[i]['titles'],
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
+
+            // Content 4
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border:
+                      Border.all(width: 3.0, color: Color.fromARGB(0, 0, 0, 0)),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.black12,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Widget Gambar untuk logo topi wisuda
+                          Image.asset(
+                            'assets/Images/Mic.jpg', // pastikan file logo ada di folder assets
+                            width: 24, // Sesuaikan ukuran gambar
+                            height: 24,
+                          ),
+                          SizedBox(
+                              width:
+                                  10), // Memberikan jarak antara logo dan teks
+                          Text(
+                            'The Hamilcast',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Fun conversation with the cast, crew, and creatives of Hamilton',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    // Button "Listen now"
+                    Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Center(
+                                          child: Container(
+                                              child: Text("Listen Now"))),
+                                      content: Text(
+                                          "Unfortunately, this feature is still on maintenance. Please contact support if the feature is still not working."),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          child: Text(
+                                            "OK",
+                                            style: TextStyle(
+                                                color: Colors.red,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pop(); // Menutup dialog
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                                buttonPressCount++;
+                              });
+                              print(
+                                  "Tombol telah dipencet sebanyak $buttonPressCount kali");
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shadowColor:
+                                  Colors.black, // Ubah warna shadow tombol
+                              elevation: 7, // Ubah elevasi tombol
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                  vertical: 10), // Ubah padding tombol
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10), // Ubah bentuk tombol menjadi lebih bulat
+                              ),
+                            ),
+                            child: Text(
+                              'Listen Now',
+                              style: TextStyle(
+                                  fontFamily: 'Arial',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                  color: Colors.blueGrey),
+                            ))),
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  // Baris ikon
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Twitter icon
+                      Image.asset(
+                        'assets/Images/X.jpg',
+                        width: 36.0,
+                        height: 36.0,
+                      ),
+                      // Facebook icon
+                      Image.asset(
+                        'assets/Images/Facebook.png',
+                        width: 36.0,
+                        height: 36.0,
+                      ),
+                      // Instagram icon
+                      Image.asset(
+                        'assets/Images/Instagram.png',
+                        width: 36.0,
+                        height: 36.0,
+                      ),
+                      // YouTube icon
+                      Image.asset(
+                        'assets/Images/Youtube.png',
+                        width: 36.0,
+                        height: 36.0,
+                      ),
+                      // Hamilton icon (bintang)
+                      Image.asset(
+                        'assets/Images/Hamilton.png',
+                        width: 36.0,
+                        height: 36.0,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ]),
