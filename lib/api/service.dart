69b1d5ef917a7459c8148b25e450667fe3fc0962
PenDZ-Alter <abxpendz45@gmail.com>
2024-10-29 16:38:31 +0700

    chore: update tasks

---
+++ lib/api/service.dart
@@ -0,0 +1,45 @@
+import 'package:odoo_rpc/odoo_rpc.dart';
+
+class OdooConnection {
+  final String _url;
+  final OdooClient _odoo;
+  var _session;
+
+  OdooConnection({required String url})
+      : _url = url,
+        _odoo = OdooClient(url);
+
+  Future<dynamic> auth(String db, String user, String pass) async {
+    try {
+      _session = await _odoo.authenticate(db, user, pass);
+      return _session;
+    } on OdooException catch (err) {
+      print('Error: $err');
+      _odoo.close();
+      return null;
+    }
+  }
+
+  Future<dynamic> getData({ required String model, required List<String> fields, int? limit, List<String>? domain }) async {
+    if (_session == null) {
+      throw Exception("Not authenticated");
+    }
+
+    try {
+      return await _odoo.callKw({
+        'model': model,
+        'method': 'search_read',
+        'args': [],
+        'kwargs': {
+          'context': {'bin_size': true},
+          'fields': fields,
+          'domain' : domain ?? [],
+          'limit': limit ?? 10,
+        },
+      });
+    } on OdooException catch (err) {
+      print('Error: $err');
+      return null;
+    }
+  }
+}
