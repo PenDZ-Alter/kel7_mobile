cc8d02e0819036e3f058dcf6e48e7a1fea454411
Ditto-Prasetyo <dittoprasetyo100@gmail.com>
2024-11-06 22:07:14 +0700

    feat: Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:tugas1_ui/pages/dashboard.dart';
+import 'package:tugas1_ui/pages/login.dart';
 
 void main() async {
   await dotenv.load(fileName: "assets/.env");
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: const LoginPage(),
     );
   }
 }
